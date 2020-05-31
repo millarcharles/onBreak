e36f13c8a0d27c2d728af751d3726360eda254bb
carlos millar <ch.cmillar@gmail.com>
2020-05-31 14:42:38 -0400

    funcionalista y add

--- onBreak/ModCliente/ClienteLista.xaml.cs
+++ onBreak/ModCliente/ClienteLista.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using onBreak_DAL;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.Entity;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,6 +23,10 @@ namespace onBreak.ModCliente
     /// </summary>
     public partial class ClienteLista : Window
     {
+        private readonly ICollectionView _clienteView;
+        OnBreakEntities _context = new OnBreakEntities();
+
+
         public ClienteLista()
         {
             InitializeComponent();
@@ -31,6 +38,9 @@ namespace onBreak.ModCliente
             System.Windows.Data.CollectionViewSource clienteViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("clienteViewSource")));
             // Load data by setting the CollectionViewSource.Source property:
             // clienteViewSource.Source = [generic data source]
+            _context.Configuration.ProxyCreationEnabled = false;
+            actualizarGrid();
+;
         }
 
         public void actualizarGrid() 
@@ -44,9 +54,9 @@ namespace onBreak.ModCliente
                                             NombreContacto = c.NombreContacto,
                                             MailContacto = c.MailContacto,
                                             Direccion = c.Direccion,
-                                            Fono = c.Telefono,
-                                            Tip = c.TipoEmpresa,
-                                            Actividad =c.ActividadEmpresa,
+                                            Telefono = c.Telefono,
+                                            TipoEmpresa = c.TipoEmpresa.Descripcion,
+                                            ActividadEmpresa = c.ActividadEmpresa.Descripcion,
 
                 } );
 
@@ -58,6 +68,15 @@ namespace onBreak.ModCliente
 
         private void ClientesGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            foreach (var item in e.AddedItems) { 
+            
+            }
+            
+        }
+
+        private void ClientesGrid_SelectedCellsChanged(object sender, SelectedCellsChangedEventArgs e)
+        {
+
 
         }
     }
---
+++ onBreak/ModCliente/ClienteViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace onBreak.ModCliente
+{
+    public class ClienteViewModel
+    {
+
+        private readonly ICollectionView _clienteView;
+
+        public ICollectionView Cliente {
+
+            get { return _clienteView; }
+        }
+
+
+    }
+}
